60cdacf65225b109a143c43b65ba6ba747acdcea
Harish <106340752+Harishharris@users.noreply.github.com>
2023-02-19 12:20:27 +0530

    Update Note.jsx
    Updated replace error

--- client/src/components/Note/Note.jsx
+++ client/src/components/Note/Note.jsx
@@ -3,9 +3,12 @@ import { Link, Route } from 'react-router-dom'
 
 export default function Note({ note }) {
 	const date = new Date()
+	if (!note) {
+		<h1>No Note</h1>
+	}
 	return (
 		<section className='each-note'>
-			<h1>{note.title.replace(note.title[0], note.title[0].toUpperCase())}</h1>
+			<h1>{note.title.replace(not, note.title[0].toUpperCase())}</h1>
 			<p>{note.body.length < 30 ? note.body : note.body.substring(0, 120) + '...'}</p>
 			<Link to={`/note/${note._id}`}>
 				<button>Read More</button>
