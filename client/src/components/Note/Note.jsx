dc1276627314c93fd0cb65f53da0e457fedd259a
Harish <106340752+Harishharris@users.noreply.github.com>
2023-02-19 12:44:24 +0530

    Update Note.jsx
    bug fixes

--- client/src/components/Note/Note.jsx
+++ client/src/components/Note/Note.jsx
@@ -8,8 +8,8 @@ export default function Note({ note }) {
 	}
 	return (
 		<section className='each-note'>
-			<h1>{note.title.replace(note.title[0], note.title[0].toUpperCase())}</h1>
-			<p>{note.body.length < 30 ? note.body : note.body.substring(0, 120) + '...'}</p>
+			<h1>{note.title}</h1>
+			<p>{note.body}</p>
 			<Link to={`/note/${note._id}`}>
 				<button>Read More</button>
 			</Link>
